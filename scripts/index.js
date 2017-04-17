64ea0e7b35697607e4fcf70318b9c92b71507651
HubertS95 <hubert.sielecki95@gmail.com>
2017-04-17 10:49:13 +0200

    przesuwanie bloczkow

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,41 @@
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover();
 });
+$(function () {
+
+    $("#draggable").draggable({
+        helper: "clone",
+        cursor: 'move'
+    });
+    $("#draggable1").draggable({
+        helper: "clone",
+        cursor: 'move'
+    });
+    $("#draggable2").draggable({
+        helper: "clone",
+        cursor: 'move'
+    });
+     $("#draggable3").draggable({
+        helper: "clone",
+        cursor: 'move'
+    });
+    $("#container").droppable({
+        drop: function (event, ui) {
+            var $canvas = $(this);
+            if (!ui.draggable.hasClass('canvas-element')) {
+                var $canvasElement = ui.draggable.clone();
+                $canvasElement.addClass('canvas-element');
+                $canvasElement.draggable({
+                    containment: '#container'
+                });
+                $canvas.append($canvasElement);
+                $canvasElement.css({
+                    left: (ui.position.left),
+                    top: (ui.position.top),
+                    position: 'absolute'
+                });
+            }
+        }
+    });
+
+});
